23a6a1b33faea28b811d721e6eb0ce7ed213d82f
matthewmcateer2 <matthewmca@hotmail.co.uk>
2020-08-20 04:46:59 +0100

    Header and footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,16 +12,37 @@
         <nav class="container mx-auto flex items-center justify-between px-4 py-6">
             <div class="flex items-center">
                 <a href="/">
-                    <img src="/laracasts-logo.svg" alt="laracasts" class="w-32 flex-none">
+                    <img src="/igdb.png" alt="laracasts" class="w-32 flex-none">
 
                 </a>
-                <ul class="flex">
+                <ul class="flex ml-16 space-x-8">
                     <li><a href="#" class="hover:text-gray-400">Games</a></li>
                     <li><a href="#" class="hover:text-gray-400">Reviews</a></li>
                     <li><a href="#" class="hover:text-gray-400">Coming Soon</a></li>
                 </ul>
             </div>
+            <div class="flex items-center">
+                <div class="relative">
+                        <input type="text" class="bg-gray-800 text-sm rounded-full focus:outline-none focus:shadow-outline w-64 px-3 py-1" placeholder="Search...">
+                        <div class="absoulte top-0">
+
+                        </div>
+                </div>
+                <div class="ml-6">
+                    <a href="#"><img src="/avatar.jpg" alt="avatar" class="rounded-full w-8" ></a>
+                </div>
+            </div>
         </nav>
     </header>
+
+    <main class="py-8">
+        @yield('content')
+    </main>
+
+    <footer class="border-t border-gray-800">
+        <div class="container mx-auto px-4 py-6">
+            Powered By <a href="#" class="underline hover:text-gray-400">IGDB API</a>
+        </div>
+    </footer>
 </body>
 </html>
